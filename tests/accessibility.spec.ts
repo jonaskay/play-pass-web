356d1aae56cd689c41ab15a6b2d5412e356e6415
Joonas Kykkänen <jonaskay@iki.fi>
2025-09-16 19:19:42 +0300

    Add support for dark mode

--- tests/accessibility.spec.ts
+++ tests/accessibility.spec.ts
@@ -2,10 +2,22 @@ import { test, expect } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
 
 test.describe("Accessibility", () => {
-  test("should not have any automatically detectable accessibility issues", async ({
+  test("should not have any automatically detectable accessibility issues in light mode", async ({
     page,
   }) => {
     await page.goto("http://localhost:4321/");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+
+  test("should not have any automatically detectable accessibility issues in dark mode", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:4321/");
+
+    await page.emulateMedia({ colorScheme: "dark" });
 
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
